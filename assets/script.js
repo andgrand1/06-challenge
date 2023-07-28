7c48b6ef85c48df37b2e60cc824ae030e9e8b46e
andgrand1 <129132408+andgrand1@users.noreply.github.com>
2023-07-27 21:02:31 -0700

    tweaked JS, now displaying temp for city

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,8 @@ var userInput = document.getElementById("search-input")
 
 buttonClick.addEventListener('click', handleName);
 
-function handleName(){
+function handleName(event){
+  event.preventDefault()
   console.log(userInput.value)
   grabWeather(userInput.value)
 }
@@ -25,4 +26,25 @@ function grabWeather(cityName){
 
 function displayWeather(weather){
   console.log(weather)
+ var todaySection = document.querySelector("#today")
+ var cityTitle = document.createElement("h1")
+ cityTitle.textContent = weather[0].name
+ todaySection.appendChild(cityTitle)
+ fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+weather[0].lat+"&lon="+weather[0].lon+"&appid="+apiKey+"&units=imperial")
+ .then(res => {
+  return res.json()
+  
+ }) .then(data => {
+  console.log(data)
+  var cityWeather = document.createElement("p")
+  cityWeather.textContent = "temperature: "+data.list[0].main.temp
+  todaySection.appendChild(cityWeather)
+  for (var i = 0; i < data.list.length; i+= 7) {
+  console.log(data.list[i])
+  // var cityWeather = document.createElement("p")
+  // cityWeather.textContent = data.list[i].main.temp
+  
+  }
+ })
 }
+
