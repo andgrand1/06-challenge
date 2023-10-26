91d049a640e843aecd71b7ff32c69fd63a4a058a
andgrand1 <andygranda63@gmail.com>
2023-10-26 11:28:17 -0700

    added 5 day forecast and css changes

--- assets/script.js
+++ assets/script.js
@@ -18,11 +18,15 @@ function handleName(event){
 function grabWeather(cityName){
   var newCityName = cityName.replaceAll(" ", "%20");
   console.log(newCityName)
-  fetch(baseUrl+"q="+ newCityName+"&appid="+apiKey)
+  
+  
+  fetch(baseUrl+"q="+ newCityName+"&limit=1&appid="+apiKey)
     .then(weather => {
       return weather.json()
     }).then(displayWeather)
 }
+
+
 
 function displayWeather(weather){
   console.log(weather)
@@ -30,6 +34,9 @@ function displayWeather(weather){
  var cityTitle = document.createElement("h1")
  cityTitle.textContent = weather[0].name
  todaySection.appendChild(cityTitle)
+
+ var forecastSection = document.querySelector("#forecast");
+
  fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+weather[0].lat+"&lon="+weather[0].lon+"&appid="+apiKey+"&units=imperial")
  .then(res => {
   return res.json()
@@ -37,12 +44,29 @@ function displayWeather(weather){
  }) .then(data => {
   console.log(data)
   var cityWeather = document.createElement("p")
-  cityWeather.textContent = "temperature: "+data.list[0].main.temp
+  cityWeather.textContent = "Todays temperature: "+data.list[0].main.temp
   todaySection.appendChild(cityWeather)
-  for (var i = 0; i < data.list.length; i+= 7) {
-  console.log(data.list[i])
-  // var cityWeather = document.createElement("p")
-  // cityWeather.textContent = data.list[i].main.temp
+  for (var i = 0; i < data.list.length; i += 8) {
+    var forecastData = data.list[i];
+    var date = new Date(forecastData.dt * 1000);
+    var day = date.toDateString().split(" ")[0];
+
+    var forecastItem = document.createElement("div");
+    forecastItem.className = "forecast-item";
+
+    var dayElement = document.createElement("p");
+    dayElement.textContent = day;
+    forecastItem.appendChild(dayElement);
+
+    var tempElement = document.createElement("p");
+    tempElement.textContent = "Temperature: " + forecastData.main.temp + "°F";
+    forecastItem.appendChild(tempElement);
+
+    var descriptionElement = document.createElement("p");
+    descriptionElement.textContent = "Description: " + forecastData.weather[0].description;
+    forecastItem.appendChild(descriptionElement);
+
+    forecastSection.appendChild(forecastItem);
   
   }
  })
